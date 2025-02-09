79dbcbb86fc8c7a71a6e51d2e554a6b4854b5f60
zang3tsu88 <maximenya.a@gmail.com>
2025-02-09 16:08:14 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      // input: {},
+    },
+  },
+});
