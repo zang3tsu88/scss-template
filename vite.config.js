904da432e1913f1f283695f437d18cbbb7b8d197
zang3tsu88 <maximenya.a@gmail.com>
2025-02-13 16:13:26 +0300

    upgrade template

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import htmlPurge from 'vite-plugin-purgecss';
 
 export default defineConfig({
   root: resolve(__dirname, 'src'),
+  plugins: [htmlPurge],
   build: {
     outDir: resolve(__dirname, 'dist'),
     emptyOutDir: true,
